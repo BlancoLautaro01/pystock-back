09fc629a2740a9438b1b214ce14dfc87ce9742ef
Luis Favatier <favatier@gmail.com>
2021-10-01 13:36:37 -0300

    test de la implementacion de user_mongo_dao.py

---
+++ test/test_user_mongo_dao.py
@@ -0,0 +1,35 @@
+from app.user_mongo_dao import *
+import pytest
+
+
+def before_each():
+    drop_users()
+
+def afeter_each():
+    drop_users()
+
+def test_user_exist():
+    before_each()
+    assert user_exist("luis@gmail.com") == False
+    insert_user("luis@gmail.com","1234")
+    assert user_exist("luis@gmail.com") == True
+    afeter_each()
+
+def test_insert_user():
+    before_each()
+    assert  user_exist("nomail@mail.com") == False
+    insert_user("nomail@mail.com","1234")
+    assert user_exist("nomail@mail.com") == True
+    afeter_each()
+
+def test_get_password():
+    before_each()
+    insert_user("nomail@mail.com","1234")
+    assert get_password("nomail@mail.com") == "1234"
+    afeter_each()
+
+def test_get_id():
+    before_each()
+    insert_user("nomail@mail.com","1234")
+    assert get_id("nomail@mail.com") is not None
+    afeter_each()
