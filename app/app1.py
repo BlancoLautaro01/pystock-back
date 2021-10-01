ebce12e9740288124cd37f5dd223dc22b44631bb
Luis Favatier <favatier@gmail.com>
2021-10-01 19:30:57 -0300

    login post

--- app/app1.py
+++ app/app1.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from connection import get_users, insert_user
+from login_manager import *
 
 app = Flask(__name__)
 CORS(app)
@@ -19,6 +20,21 @@ def users():
 @app.route('/users', methods=["POST"])
 def create_user():
     return jsonify(insert_user(request.form["username"], request.form["password"]))
+
+@app.route('/login', methods=["POST"])
+def login():
+     #retorno de la funcion
+    response = None
+
+    credentials = request.form
+    email = credentials["email"]
+    password = credentials["password"]
+    checked = check_login(email, password)
+    if checked:
+        response = {"id", "token"}
+    else:
+        response = {}
+    return response
 
 
 if __name__ == '__main__':
