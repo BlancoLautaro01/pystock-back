938e502c216b19b6d5cefc3dcdfddae296df453f
Luis Favatier <favatier@gmail.com>
2021-10-16 14:09:02 -0300

    product_controller: Se agrego documentacion

--- pystock/app/routes/product_controller.py
+++ pystock/app/routes/product_controller.py
@@ -7,6 +7,24 @@ product_controller = Blueprint('product_controller', __name__)
 
 @product_controller.route('/setProduct', methods=["POST"])
 def set_product():
+    """
+    '/setProduct', methods=["POST"]
+    Request:{
+                "cod":Int,      //codigo unico del producto
+                "name":String,  //nombre del producto
+                "price":Int     //precio unitario del producto
+                "desc":String   //descripcion del producto
+            }
+
+    :response:{
+                "id":Int        //id del producto
+                "cod":Int,      //codigo unico interno del producto
+                "name":String,  //nombre del producto
+                "price":Int     //precio unitario del producto
+                "desc":String   //descripcion del producto
+              }
+    Nota: cod, es el codigo interno de la empresa que le asigna al producto.
+    """
     credentials = request.json
     cod = credentials["cod"]
     name = credentials["name"]
@@ -23,6 +41,23 @@ def set_product():
 
 @product_controller.route('/editProduct/<product_id>', methods=["PUT"])
 def edit_product(product_id):
+    """
+    '/editProduct/<product_id>', methods=["PUT"]
+    Request:{
+                "cod":Int,      //codigo unico del producto
+                "name":String,  //nombre del producto
+                "price":Int     //precio unitario del producto
+                "desc":String   //descripcion del producto
+            }
+
+    :response:{
+                "id":Int        //id del producto
+                "cod":Int,      //codigo unico interno del producto
+                "name":String,  //nombre del producto
+                "price":Int     //precio unitario del producto
+                "desc":String   //descripcion del producto
+              }
+    """
     credentials = request.json
     cod = credentials["cod"]
     name = credentials["name"]
@@ -36,10 +71,19 @@ def edit_product(product_id):
 
 @product_controller.route('/getProducts')
 def products():
+    """
+    '/getProducts' metodo tipo GET
+    Retorna un json con todos los productos.
+    """
     return jsonify(get_products()), 200
 
 
 @product_controller.route('/deleteProduct/<product_id>', methods=["DELETE"])
 def delete_a_product(product_id):
+    """
+    '/deleteProduct/<product_id>', methods=["DELETE"]
+    Borra un producto con id: <product_id>'
+    Retorna "Deleted" con codigo HTPP 204
+    """
     delete_product(product_id)
     return "Deleted", 204
