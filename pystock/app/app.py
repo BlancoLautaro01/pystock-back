faf2114066c6e2b4829be566ee46d4840da35707
Lautaro Blanco <blanco.lautaro@outlook.com>
2021-10-15 22:56:48 -0300

    app: Bluepring Product agregado

--- pystock/app/app.py
+++ pystock/app/app.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 
 from pystock.app.routes.user_controller import user_controller
+from pystock.app.routes.product_controller import product_controller
 
 # App creation + Cors
 app = Flask(__name__)
@@ -9,5 +10,6 @@ CORS(app)
 
 # Routes register
 app.register_blueprint(user_controller)
+app.register_blueprint(product_controller)
 
 app.run(port=4000)
