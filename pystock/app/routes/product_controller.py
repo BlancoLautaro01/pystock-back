9a2f4e51ee490f227fc3cce2cd8faba3e79007e9
Lautaro Blanco <blanco.lautaro@outlook.com>
2021-10-15 22:57:13 -0300

    product_controller: dix endpoint

--- pystock/app/routes/product_controller.py
+++ pystock/app/routes/product_controller.py
@@ -4,15 +4,18 @@ from pystock.app.services.product_service import *
 
 product_controller = Blueprint('product_controller', __name__)
 
+
 @product_controller.route('/setProduct', methods=["POST"])
 def set_product():
     credentials = request.json
-    id_code = credentials["id_code"]
+    code = credentials["cod"]
     name = credentials["name"]
     price = credentials["price"]
-    description = credentials["description"]
+    description = credentials["desc"]
 
-    if product_exist(id_code):
-        return "Product with the same id already exist", 500
-    product = insert_product(id_code, name, price, description)
+    if product_exist(code):
+        return "Product with the same id already exist", 400
+
+    product = insert_product(code, name, price, description)
+
     return jsonify(product), 201
