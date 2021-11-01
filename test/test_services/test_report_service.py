a085adb372e65d5e5a6179ce3c9d00c63c2042da
Lautaro Blanco <blanco.lautaro@outlook.com>
2021-11-01 17:05:07 -0300

    test_report_service: getReport test agregado

--- test/test_services/test_report_service.py
+++ test/test_services/test_report_service.py
@@ -1,5 +1,5 @@
 from pystock.app.services.movements_service import *
-from pystock.app.services.product_service import insert_product, delete_product, get_by_cod
+from pystock.app.services.product_service import insert_product, get_all_codes
 from pystock.app.services.report_service import get_report
 
 
@@ -12,12 +12,24 @@ def before_each():
 def after_each():
     drop_collection()
 
-#
-# def test_report():
-#     set_movement("COD1", 10, True)
-#     set_movement("COD1", "5", False)
-#
-#     set_movement("COD2", 10, True)
-#
-#     assert len(get_report()) == 3
-#     assert report[0] is not None
+
+def test_report():
+    before_each()
+
+    set_movement("COD1", 10, True)
+    set_movement("COD1", "5", False)
+
+    set_movement("COD2", 10, True)
+
+    report = get_report()
+    assert len(report) == 3
+    assert report[0]["cod"] == "COD1"
+    assert report[0]["total"] == 5
+
+    assert report[1]["cod"] == "COD2"
+    assert report[1]["total"] == 10
+
+    assert report[2]["cod"] == "COD3"
+    assert report[2]["total"] == 0
+
+    after_each()
